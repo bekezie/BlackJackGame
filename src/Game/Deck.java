6dc0edb1893d66e5c4638f89108915741a7086a9
bekezie <73091370+bekezie@users.noreply.github.com>
2020-12-30 14:53:41 -0800

    Update Deck.java

--- src/Game/Deck.java
+++ src/Game/Deck.java
@@ -5,63 +5,47 @@ import java.util.Collections;
 
 public class Deck {
     private ArrayList<Card> deck;
-    private ArrayList<Card> discard;
+    private ArrayList<Card> discard = new ArrayList<>();
     public Deck()
     {
 
+        // used to add face values into name array list
+        int i = 2;
+
+        // helper variable used to locate suit name when adding to deck arraylist
+        int k = 0;
+
         deck = new ArrayList<>();
-        deck.add(new Card("Clubs", "2"));
-        deck.add(new Card("Clubs", "3"));
-        deck.add(new Card("Clubs", "4"));
-        deck.add(new Card("Clubs", "5"));
-        deck.add(new Card("Clubs", "6"));
-        deck.add(new Card("Clubs", "7"));
-        deck.add(new Card("Clubs", "8"));
-        deck.add(new Card("Clubs", "9"));
-        deck.add(new Card("Clubs", "10"));
-        deck.add(new Card("Diamonds", "2"));
-        deck.add(new Card("Diamonds", "3"));
-        deck.add(new Card("Diamonds", "4"));
-        deck.add(new Card("Diamonds", "5"));
-        deck.add(new Card("Diamonds", "6"));
-        deck.add(new Card("Diamonds", "7"));
-        deck.add(new Card("Diamonds", "8"));
-        deck.add(new Card("Diamonds", "9"));
-        deck.add(new Card("Diamonds", "10"));
-        deck.add(new Card("Hearts", "2"));
-        deck.add(new Card("Hearts", "3"));
-        deck.add(new Card("Hearts", "4"));
-        deck.add(new Card("Hearts", "5"));
-        deck.add(new Card("Hearts", "6"));
-        deck.add(new Card("Hearts", "7"));
-        deck.add(new Card("Hearts", "8"));
-        deck.add(new Card("Hearts", "9"));
-        deck.add(new Card("Hearts", "10"));
-        deck.add(new Card("Spades", "2"));
-        deck.add(new Card("Spades", "3"));
-        deck.add(new Card("Spades", "4"));
-        deck.add(new Card("Spades", "5"));
-        deck.add(new Card("Spades", "6"));
-        deck.add(new Card("Spades", "7"));
-        deck.add(new Card("Spades", "8"));
-        deck.add(new Card("Spades", "9"));
-        deck.add(new Card("Spades", "10"));
-        deck.add(new Card("Clubs", "King"));
-        deck.add(new Card("Clubs", "Queen"));
-        deck.add(new Card("Clubs", "Jack"));
-        deck.add( new Card("Clubs", "Ace"));
-        deck.add(new Card("Diamonds", "King"));
-        deck.add(new Card("Diamonds", "Queen"));
-        deck.add(new Card("Diamonds", "Jack"));
-        deck.add( new Card("Diamonds", "Ace"));
-        deck.add(new Card("Hearts", "King"));
-        deck.add(new Card("Hearts", "Queen"));
-        deck.add(new Card("Hearts", "Jack"));
-        deck.add( new Card("Hearts", "Ace"));
-        deck.add(new Card("Spades", "King"));
-        deck.add(new Card("Spades", "Queen"));
-        deck.add(new Card("Spades", "Jack"));
-        deck.add( new Card("Spades", "Ace"));
+        ArrayList<String> suit = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
+
+        suit.add("Clubs");
+        suit.add("Diamonds");
+        suit.add("Hearts");
+        suit.add("Spades");
+
+        for(int j = 0; j < 9; j++)
+        {
+            String s = String.valueOf(i);
+            name.add(s);
+            i++;
+        }
+        name.add("King");
+        name.add("Queen");
+        name.add("Jack");
+        name.add("Ace");
+
+        i = 0;
+        for(int j = 0; j < 52; j++)
+        {
+
+            if(i >= 13) {
+                i = 0;
+                k++;
+            }
+            deck.add(new Card(suit.get(k), name.get(i)));
+            i++;
+        }
 
     }
     public void PrintDeck()
@@ -103,21 +87,22 @@ public class Deck {
         catch (Exception e)
         {
             System.out.println("The deck is empty!");
-            System.out.println("Reshuffling from discard.");
-            while (discard.size() >= 1)
+            System.out.println("Reshuffling from discarded deck.");
+            while (discard.size() > 0)
             {
                 deck.add(discard.remove(0));
             }
             ShuffleDeck();
-            return RemoveCard();
+            aCard = new Card(deck.remove(0));
+            return aCard;
         }
+
     }
 
-    public ArrayList DisCard(Card aCard)
+    public void DisCard(Card aCard)
     {
-        discard = new ArrayList<>();
         discard.add(aCard);
-        return discard;
+        //return discard;
     }
 
 }
